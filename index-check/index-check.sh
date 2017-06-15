f3d4a5754d8b1d2728a9fde2e3f0afdb81c833f6
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2017-06-15 18:43:12 +0200

    New version which directly produces YAML

--- index-check/index-check.sh
+++ index-check/index-check.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+#Get the IDs
 SEARCH=`bin/librecat export search --bag publication to YAML | grep "^_id:" | tr -d _id:\'\ `
 BACKUP=`bin/librecat export backup --bag publication to YAML | grep "^_id:" | tr -d _id:\'\ `
 
-echo ${SEARCH[@]} ${BACKUP[@]} | tr ' ' '\n' | sort | uniq -u
+mkdir -p ./tmp
+# Remove IDs from list that are in both stores
+echo ${SEARCH[@]} ${BACKUP[@]} | tr ' ' '\n' | sort | uniq -u > ./tmp/id-list.txt
+# Count
+LINES=`wc -l ./tmp/id-list.txt`
+# Get YAML entries for IDs, that are missing in one store
+echo "Got ${LINES} IDs differenc, this might take some time"
+xargs -n 1 -a ./tmp/id-list.txt bin/librecat publication get > ./tmp/not-indexed.yaml
+rm ./tmp/id-list.txt
